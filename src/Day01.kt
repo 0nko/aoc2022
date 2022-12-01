4cd45703107377a05ee582ef7ed876c43b64f1ed
Ondrej Ruttkay <ruttkay@gmail.com>
2022-12-01 21:50:12 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,26 @@
+fun getSums(input: List<String>): List<Int> {
+    val sums = mutableListOf(0)
+    input.forEach {
+        if (it.isBlank()) {
+            sums += 0
+        } else {
+            sums[sums.lastIndex] += it.toInt()
+        }
+
+    }
+    return sums
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return getSums(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getSums(input).sorted().reversed().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
